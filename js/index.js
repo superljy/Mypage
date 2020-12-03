201146385d5a6a6af2e16249b293c619c1891bb1
superljy0809@163.com <superljy0809@163.com>
2020-12-03 20:52:11 +0800

    oil 0.1

--- js/index.js
+++ js/index.js
@@ -3,8 +3,16 @@
     envelope.addEventListener('click', () => {
         let fontPage = document.querySelector('.font-page');
         fontPage.style.opacity = '0';
+        setTimeout(() => {
+            fontPage.style.zIndex = '-1';
+        }, 1200);
     })
-    // let mySwiper = new Swiper('.swiper-container', {
-    //     direction: 'vertical'
-    // })
+
+    let nextPage = document.querySelector('.next-page');
+    nextPage.addEventListener('click', () => {
+        let page1 = document.querySelector('.page1');
+        let page2 = document.querySelector('.page2');
+        page1.style.opacity = '0';
+        page2.style.opacity = '1';
+    })
 })();
