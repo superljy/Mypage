806cc1ce008dfc2b81f36d4bdec6da3cbb7082b6
superljy0809@163.com <superljy0809@163.com>
2020-12-03 19:23:57 +0800

    oil

---
+++ js/index.js
@@ -0,0 +1,10 @@
+(function () {
+    let envelope = document.querySelector('.envelope');
+    envelope.addEventListener('click', () => {
+        let fontPage = document.querySelector('.font-page');
+        fontPage.style.opacity = '0';
+    })
+    // let mySwiper = new Swiper('.swiper-container', {
+    //     direction: 'vertical'
+    // })
+})();
